8935123fbcd2447e58850bd285e2f80ed7f5be84
sudheer <sudheer694@gmail.com>
2024-07-03 07:09:15 +0530

    Updated the CarMake.java as per the review comments requested in Pull Request #1

--- pluto/src/test/java/org/iit/mmp/tests/CarMake.java
+++ pluto/src/test/java/org/iit/mmp/tests/CarMake.java
@@ -1,19 +1,21 @@
 package org.iit.mmp.tests;
 
+import java.util.ArrayList;
+
 public class CarMake {
 
 	public static void main(String[] args) {
 		
-		String[] cars = new String[5];
+		ArrayList<String> cars = new ArrayList<String>();
 		
-		cars [0]= "toyota";
-		cars[1] = "Jeep";
-		cars [2]= "Kia";
-		cars[3] = "Mazda";
-		cars[4] = "Benz";
+		cars.add("toyota");
+		cars.add("Jeep");
+		cars.add("Kia");
+		cars.add("Mazda");
+		cars.add("Benz");
 		
-		System.out.println("Total Cars Present : "+cars.length);
-		System.out.println("cars[4] "+cars[4]);
+		System.out.println("Total Cars Present : "+cars.size());
+		System.out.println("cars[4] "+cars.get(4));
 		
 		for(String carmake : cars)
 		{
